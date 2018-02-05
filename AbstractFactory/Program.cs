750066379600cf1d8fa516751ea88ffb89c21f68
Gleb Skripnikov <gspostmail@gmail.com>
2018-02-06 02:09:53 +0400

    AddAll

--- AbstractFactory/Program.cs
+++ AbstractFactory/Program.cs
@@ -11,6 +11,7 @@ namespace AbstractFactory
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, world!");
+            Console.WriteLine("AbstractFactory");
         }
     }
 }
