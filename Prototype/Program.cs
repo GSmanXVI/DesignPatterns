da35ecf39018a2656f08464bf789ef373318d6e7
Gleb Skripnikov <gspostmail@gmail.com>
2018-02-06 07:02:32 +0400

    Prototype 2

--- Prototype/Program.cs
+++ Prototype/Program.cs
@@ -87,12 +87,35 @@ namespace Prototype
             CharacterManager characterManager = new CharacterManager();
             Character character;
 
-            Console.Write("Choose your class (1 - Warrior, 2 - Theif, 3 - Mage): ");
-            Enum.TryParse(Console.ReadLine(), out Class classChoise);
-            character = characterManager[classChoise] ?? characterManager[Class.Warrior];
+            
+            while (true)
+            {
+                Console.Write("Choose your class (1 - Warrior, 2 - Theif, 3 - Mage): ");
+                Enum.TryParse(Console.ReadKey().KeyChar.ToString(), out Class classChoise);
+                try
+                {
+                    character = characterManager[classChoise];
+                    Console.Clear();
+                    break;
+                }
+                catch (KeyNotFoundException)
+                {
+                    Console.Clear();
+                }
+            }
 
-            Console.Write("Write your name: ");
-            character.Name = Console.ReadLine();
+            while (true)
+            {
+                Console.Write("Write your name: ");
+                character.Name = Console.ReadLine();
+                if (String.IsNullOrWhiteSpace(character.Name))
+                {
+                    Console.Clear();
+                    Console.WriteLine("Name can't be empty!");
+                }
+                else break;
+            }
+
 
             int additionalPoints = 3;
             while (additionalPoints != 0)
@@ -101,22 +124,22 @@ namespace Prototype
                 character.Info();
                 Console.WriteLine($"You have {additionalPoints} additional points!");
                 Console.Write("Choose stat to improve (1 - STR, 2 - DEX, 3 - INT): ");
-                Int32.TryParse(Console.ReadLine(), out int skillChoise);
+                Int32.TryParse(Console.ReadKey().KeyChar.ToString(), out int skillChoise);
                 switch (skillChoise)
                 {
                     case 1:
                         character.Strength++;
+                        additionalPoints--;
                         break;
                     case 2:
                         character.Dexterity++;
+                        additionalPoints--;
                         break;
                     case 3:
                         character.Intellect++;
-                        break;
-                    default:
+                        additionalPoints--;
                         break;
                 }
-                additionalPoints--;
             }
 
             Console.Clear();
