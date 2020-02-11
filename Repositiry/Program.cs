f40ae5e8dba2bd3fb00cb2119a9153f60b80c249
Gleb Skripnikov <gspostmail@gmail.com>
2020-02-11 14:13:16 +0400

    Repository

---
+++ Repositiry/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+
+namespace Repositiry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var unitOfWork = new UnitOfWork();
+            unitOfWork.People.Create(new Person 
+            { 
+                Id = "1",
+                FullName = "Gleb Skripnikov",
+                Age = 26
+            });
+            unitOfWork.People.Create(new Person
+            {
+                Id = "2",
+                FullName = "Firdovsy Babatov",
+                Age = 40
+            });
+
+            unitOfWork.Cards.Create(new Card
+            {
+                OwnerId = "1",
+                Number = "1111222233334444",
+                Pin = 1234,
+                Balance = 100000
+            });
+
+            unitOfWork.Save();
+        }
+    }
+
+    class Person
+    {
+        public string Id { get; set; }
+        public string FullName { get; set; }
+        public int Age { get; set; }
+    }
+
+    class Card
+    {
+        public string OwnerId { get; set; }
+        public string Number { get; set; }
+        public int Pin { get; set; }
+        public decimal Balance { get; set; }
+    }
+
+    //CRUD (Create, Read, Update, Delete)
+    interface IRepositiry<T> where T : class
+    {
+        //Read
+        T Get(string id);
+        IEnumerable<T> GetAll();
+
+        //Create
+        void Create(T item);
+
+        //Update
+        void Update(string id, T item);
+
+        //Delete
+        void Delele(string id);
+    }
+
+    abstract class Repositiry<T> : IRepositiry<T> where T : class
+    {
+        static protected Dictionary<string, T> storage = new Dictionary<string, T>();
+
+        virtual public T Get(string id)
+        {
+            storage.TryGetValue(id, out T item);
+            return item;
+        }
+
+        virtual public IEnumerable<T> GetAll()
+        {
+            return storage.Select(x => x.Value);
+        }
+
+        abstract public void Create(T item);
+
+        virtual public void Update(string id, T item)
+        {
+            storage[id] = item;
+        }
+
+        virtual public void Delele(string id)
+        {
+            storage.Remove(id);
+        }
+    }
+
+    class PersonRepositiory : Repositiry<Person>
+    {
+        override public void Create(Person item)
+        {
+            storage.Add(item.Id, item);
+        }
+    }
+
+    class CardRepositiory : Repositiry<Card>
+    {
+        override public void Create(Card item)
+        {
+            storage.Add(item.Number, item);
+        }
+    }
+
+    class UnitOfWork
+    {
+        public PersonRepositiory People { get; set; } = new PersonRepositiory();
+        public CardRepositiory Cards { get; set; } = new CardRepositiory();
+
+        public void Save()
+        {
+            var peopleJson = JsonSerializer.Serialize(People.GetAll());
+            var cardsJson = JsonSerializer.Serialize(Cards.GetAll());
+
+            File.WriteAllText("people.json", peopleJson);
+            File.WriteAllText("cards.json", cardsJson);
+        }
+    }
+}
--- TemplateMethod/Program.cs
+++ TemplateMethod/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -7,132 +8,75 @@ using System.Threading.Tasks;
 
 namespace TemplateMethod
 {
-    //abstract class Command
-    //{
-    //    public abstract void Execute();
-    //    public abstract void UnExecute();
-    //}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataReader dataReader = new TxtDataReader();
+            var total = dataReader.GetProductsTotalPrice("products.txt");
+            Console.WriteLine(total);
+        }
+    }
 
-    //class CalculatorCommand : Command
-    //{
-    //    public char Operation { get; set; }
-    //    public int Operand { get; set; }
-    //    Calculator calculator;
+    //txt & csv
 
-    //    public CalculatorCommand(Calculator calculator, char operation, int operand)
-    //    {
-    //        this.calculator = calculator;
-    //        Operation = operation;
-    //        Operand = operand;
-    //    }
+    class Product
+    {
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+    }
 
-    //    public override void Execute()
-    //    {
-    //        calculator.Operation(Operation, Operand);
-    //    }
 
-    //    public override void UnExecute()
-    //    {
-    //        calculator.Operation(Undo(Operation), Operand);
-    //    }
+    abstract class DataReader
+    {
+        abstract public IEnumerable<Product> ReadProductsFromFile(string filename);
 
-    //    private char Undo(char Operation)
-    //    {
-    //        char undo;
-    //        switch (Operation)
-    //        {
-    //            case '+': undo = '-'; break;
-    //            case '-': undo = '+'; break;
-    //            case '*': undo = '/'; break;
-    //            case '/': undo = '*'; break;
-    //            default: undo = ' '; break;
-    //        }
-    //        return undo;
-    //    }
-    //}
+        virtual public decimal GetProductsTotalPrice(string filename)
+        {
+            var products = ReadProductsFromFile(filename);
+            return products.Sum(x => x.Price);
+        }
+    }
 
-    //class Calculator
-    //{
-    //    private int curr = 0;
+    class TxtDataReader : DataReader
+    {
+        public override IEnumerable<Product> ReadProductsFromFile(string filename)
+        {
+            List<Product> products = new List<Product>();
 
-    //    public void Operation(char operation, int operand)
-    //    {
-    //        switch (operation)
-    //        {
-    //            case '+': curr += operand; break;
-    //            case '-': curr -= operand; break;
-    //            case '*': curr *= operand; break;
-    //            case '/': curr /= operand; break;
-    //        }
-    //        Console.WriteLine("Current value = {0,3} (following {1} {2})",
-    //            curr, operation, operand);
-    //    }
-    //}
+            var lines = File.ReadAllLines(filename);
+            foreach (var line in lines)
+            {
+                var items = line.Split(new char[] { ' ', ':' }, StringSplitOptions.RemoveEmptyEntries);
+                products.Add(new Product
+                {
+                    Title = items[0],
+                    Price = decimal.Parse(items[1])
+                });
+            }
 
-    //class User
-    //{
-    //    // Initializers
-    //    private Calculator calculator = new Calculator();
-    //    private List<Command> commands = new List<Command>();
+            return products;
+        }
+    }
 
-    //    private int current = 0; //номер последней выполненной команды
+    class CsvDataReader : DataReader
+    {
+        public override IEnumerable<Product> ReadProductsFromFile(string filename)
+        {
+            List<Product> products = new List<Product>();
 
-    //    public void Redo(int levels)
-    //    {
-    //        Console.WriteLine("\n---- Redo {0} levels ", levels);
+            var lines = File.ReadAllLines(filename);
+            foreach (var line in lines)
+            {
+                var items = line.Split(',');
+                products.Add(new Product
+                {
+                    Title = items[0],
+                    Price = decimal.Parse(items[1])
+                });
+            }
 
-    //        // Делаем возврат операций
-    //        for (int i = 0; i < levels; i++)
-    //            if (current < commands.Count - 1)
-    //                commands[current++].Execute();
-    //    }
-
-    //    public void Undo(int levels)
-    //    {
-    //        Console.WriteLine("\n---- Undo {0} levels ", levels);
-
-    //        // Делаем отмену операций
-    //        for (int i = 0; i < levels; i++)
-    //            if (current > 0)
-    //                commands[--current].UnExecute();
-    //    }
-
-    //    public void Compute(char @operator, int operand)
-    //    {
-
-    //        // Создаем команду операции и выполняем её
-    //        Command command = new CalculatorCommand(calculator, @operator, operand);
-    //        command.Execute();
-
-    //        // Добавляем операцию к списку отмены
-    //        commands.Add(command);
-    //        current++;
-    //    }
-    //}
-
-    //class Program
-    //{
-    //    static void Main(string[] args)
-    //    {
-    //        // Создаем инициатора выполнения команды
-    //        User user = new User();
-
-    //        // Пусть он что-нибудь сделает.
-    //        user.Compute('+', 100);
-    //        user.Compute('-', 50);
-    //        user.Compute('*', 10);
-    //        user.Compute('/', 2);
-
-    //        // Отменяем 4 команды
-    //        user.Undo(2);
-
-    //        user.Compute('+', 1);
-
-    //        // Вернём 3 отменённые команды.
-    //        user.Redo(1);
-
-    //        // Ждем ввода пользователя и завершаемся.
-    //        Console.Read();
-    //    }
-    //}
+            return products;
+        }
+    }
 }
